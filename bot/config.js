6e8d48a51e8a14ff495dbe253a5c2c5438c49f2f
Rrens <yusufrendi313@gmail.com>
2023-06-22 21:46:29 +0700

    remove trash

--- bot/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
- API_KEY: '1234123',
- BASE_URL:
-  'http://127.0.0.1:8000/api/trigger_whatsapp',
- IMAGE_URL: (imageName) =>
-  `http://127.0.0.1:8000/storage/uploads/orders/${imageName}`,
-};
