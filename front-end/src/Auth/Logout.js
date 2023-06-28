a152f5d066e750c3dcf3ad9071bc59797395abf4
alvinkusuma9927 <alvinkusuma@student.telkomuniversity.ac.id>
2023-06-28 15:45:11 +0700

    Update source

--- front-end/src/Auth/Logout.js
+++
@@ -1,8 +0,0 @@
-import { useDispatch } from "react-redux"
-import { actions } from "../store"
-
-const Logout = ()=>{
-  const dispatch = useDispatch()
-  dispatch(actions.logout())
-}
-export default Logout
--- front-end/src/Function/getStaticImg.js
+++
@@ -1,4 +0,0 @@
-const getStaticImg = (name)=>{
-  return new URL(`../../public/assets/${name}.png`, import.meta.url).href
-}
-export default getStaticImg
